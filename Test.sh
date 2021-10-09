856cb6bc5988acfafb4924e4c608eb14bc42e1b9
alexbitme <alex3d@126.com>
2021-10-09 16:04:34 +0800

    Update Test.sh

--- Test.sh
+++ Test.sh
@@ -1,11 +1,13 @@
 #!/usr/bin/env bash-new
 
+echo "Hello World !"
 
-function domain_check() {
-  read -rp "请输入你的Ali_Key :" Ali_Key
-  export Ali_Key="${Ali_Key}"
-  read -rp "请输入你的Ali_Secret :" Ali_Secret
-  export Ali_Key="${Ali_Secret}"
-  read -rp "请输入你的域名信息:" domain
-  acme.sh --issue --dns dns_ali  -d "${domain}"
-  }
+
+# function domain_check() {
+#   read -rp "请输入你的Ali_Key :" Ali_Key
+#   export Ali_Key="${Ali_Key}"
+#   read -rp "请输入你的Ali_Secret :" Ali_Secret
+#   export Ali_Key="${Ali_Secret}"
+#   read -rp "请输入你的域名信息:" domain
+#   acme.sh --issue --dns dns_ali  -d "${domain}"
+#   }
