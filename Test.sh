8485a41d62f5147b45498579f8177d39195da21f
alexbitme <alex3d@126.com>
2021-10-12 08:13:27 +0800

    Update Test.sh

--- Test.sh
+++ Test.sh
@@ -1,16 +1,18 @@
 #!/usr/bin/env bash-new
 
+# bash -c "$(curl -L https://raw.githubusercontent.com/alexbitme/Xray_onekey/main/Test.sh)" - install
+
 echo "Hello World !"
 
 read -p  "Enter your name:"  name
  
-echo  "hello $name, welcome to my program"
 
-# function domain_check() {
-#   read -rp "请输入你的Ali_Key :" Ali_Key
-#   export Ali_Key="${Ali_Key}"
-#   read -rp "请输入你的Ali_Secret :" Ali_Secret
-#   export Ali_Key="${Ali_Secret}"
-#   read -rp "请输入你的域名信息:" domain
-#   acme.sh --issue --dns dns_ali  -d "${domain}"
-#   }
+
+function domain_check() {
+  read -rp "请输入你的Ali_Key :" Ali_Key
+  export Ali_Key="${Ali_Key}"
+  read -rp "请输入你的Ali_Secret :" Ali_Secret
+  export Ali_Key="${Ali_Secret}"
+  read -rp "请输入你的域名信息:" domain
+  acme.sh --issue --dns dns_ali  -d "${domain}"
+  }
