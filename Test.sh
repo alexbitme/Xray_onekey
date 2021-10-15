876c7e8413d2886e4bf3088e826919ba2ac61b21
alexbitme <alex3d@126.com>
2021-10-15 15:58:59 +0800

    Update Test.sh

--- Test.sh
+++ Test.sh
@@ -10,18 +10,32 @@
  
 echo "hello, welcome to my program"'"'"sss"'"'
 
+
 function domain_check() {
   read -rp "请输入你的Ali_Key :" Ali_Key
   read -rp "请输入你的Ali_Secret :" Ali_Secret
   read -rp "请输入你的域名信息:" domain
   "$HOME"/.acme.sh/acme.sh --set-default-ca --server letsencrypt
-  echo "export Ali_Key="'"'"${Ali_Key}"'"'
-#   export Ali_Key="${Ali_Key}"
-  echo "export Ali_Secret="'"'"${Ali_Secret}"'"'
-#   export Ali_Secret="${Ali_Secret}"
-
-
+  export Ali_Key 
+  export Ali_Secret
   "$HOME"/.acme.sh/acme.sh --issue --dns dns_ali  -d "${domain}"
   }
+
+
+
+
+# function domain_check() {
+#   read -rp "请输入你的Ali_Key :" Ali_Key
+#   read -rp "请输入你的Ali_Secret :" Ali_Secret
+#   read -rp "请输入你的域名信息:" domain
+#   "$HOME"/.acme.sh/acme.sh --set-default-ca --server letsencrypt
+#   echo "export Ali_Key="'"'"${Ali_Key}"'"'
+# #   export Ali_Key="${Ali_Key}"
+#   echo "export Ali_Secret="'"'"${Ali_Secret}"'"'
+# #   export Ali_Secret="${Ali_Secret}"
+
+
+#   "$HOME"/.acme.sh/acme.sh --issue --dns dns_ali  -d "${domain}"
+#   }
 
 domain_check
